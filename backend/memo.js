82ebbecd37116ca3687db58dd3cc2a86420a3a4d
Yong C. Jang <janggyver@gmail.com>
2018-11-25 20:03:28 -0500

    Build Basci Structure for Anonymous Board

---
+++ backend/memo.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//this will be our database's data structure
+
+const MemoSchema = new Schema(
+    {
+    id: Number,
+    message: String,
+    password: String,
+    date: ISODate(timestamps)
+    },
+    {timestamps: true}
+);
+
+//export the new Schema so we could modify it using Node.js
+module.exports = mongoose.model("Memo", MemoSchema);
